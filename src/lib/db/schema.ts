38c6b628d33627c33f15612c632730845af325a1
Yash Visal <yav2@duke.edu>
2023-09-10 17:38:10 -0400

    first push

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,8 @@
+import {pgTable, serial, text} from 'drizzle-orm/pg-core'
+
+export const chats = pgTable('chats', {
+    id: serial('id').primaryKey(),
+    pdfName: text('pdf_name').notNull(),
+    pdfUrl: text('pdf_url').notNull(),
+    
+})
